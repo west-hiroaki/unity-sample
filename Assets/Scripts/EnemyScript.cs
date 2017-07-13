a21a4db30703aac4bcd03b9c7ca3986906c14b49
hiroaki.nishi <hiroaki.nish@gu3.co.jp>
2017-07-13 11:06:02 +0900

    基本動作サンプル作成

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+
+	void OnCollisionEnter(Collision collision){
+		Debug.Log ("crush! " + collision.gameObject.name);
+
+		if (collision.gameObject.tag == "Player") {
+			Destroy (this.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/TapAddForceScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class TapAddForceScript : MonoBehaviour {
+
+	private Rigidbody rigid;
+
+	void Start () {
+		rigid = gameObject.GetComponent<Rigidbody> ();
+	}
+	
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Debug.Log ("tap!");
+			rigid.AddForce(transform.right * 500f);
+		}
+	}
+}
